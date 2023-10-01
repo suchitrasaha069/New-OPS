7edc402a34922e9b4825de17999a4e0481f7079e
Saikat Saha <64367664+Saikatsaha798@users.noreply.github.com>
2023-10-01 21:53:21 +0530

    Added a setter method
    This method will allow updating data of Employee class.

--- employeeData.java
+++ employeeData.java
@@ -2,6 +2,11 @@ class Employee{
   int emp_id;
   String emp_name;
 
+  void setData (int id, String name){
+    this.emp_id = id;
+    this.emp_name = name;
+  }
+
   Employee (int id, String name){
     this.emp_id = id;
     this.emp_name = name;
@@ -11,6 +16,8 @@ class Employee{
 public class employeeData{
   public static void main (String args[]){
     // System.out.println("This is body!");
-    Employee emp1 = new Employee(1, "Ram");
+    Employee emp1 = new Employee(1, "Ram");   // Parameterised constructor
+
+    emp1.setData(2, "Shyam");  // Using setter method
   }
 }
